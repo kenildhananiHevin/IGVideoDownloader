a58e0badb64e330ddd667f34aacc2c0471b2f50c
kenildhananiHevin <kenildhananiHevin@gmail.com>
2024-05-25 16:04:47 +0530

    google service

---
+++ app/src/main/java/vidmatinsta/downloader/fullmovie/tube/socialmedia/downloadfreevidmata/statussaver/interfaces/StoryUserListInterface.java
@@ -0,0 +1,8 @@
+package vidmatinsta.downloader.fullmovie.tube.socialmedia.downloadfreevidmata.statussaver.interfaces;
+
+import com.cashloan.myapplication.igvideodownloader.model.story.StoryTray;
+
+public interface StoryUserListInterface {
+
+    void storyUserListClick(int i, StoryTray trayModel);
+}
