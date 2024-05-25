508afd376c312bb50ee2d08fb4962af1132f8e5e
kenildhananiHevin <kenildhananiHevin@gmail.com>
2024-05-25 16:28:11 +0530

    splash logo change

--- app/src/main/java/vidmatinsta/downloader/fullmovie/tube/socialmedia/downloadfreevidmata/statussaver/interfaces/StoryUserListInterface.java
+++
@@ -1,8 +0,0 @@
-package vidmatinsta.downloader.fullmovie.tube.socialmedia.downloadfreevidmata.statussaver.interfaces;
-
-import com.cashloan.myapplication.igvideodownloader.model.story.StoryTray;
-
-public interface StoryUserListInterface {
-
-    void storyUserListClick(int i, StoryTray trayModel);
-}
